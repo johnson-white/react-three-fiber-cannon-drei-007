0006a8140bc69aad40fb60a0a1f839df9699e663
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-05-26 16:05:54 +0100

    Add styling to NavBar, add some content to Home page

--- src/Components/Pages/DOMComponents/NavBar/index.js
+++ src/Components/Pages/DOMComponents/NavBar/index.js
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
+import style from "./NavBar.module.css";
 
 function NavBar({ updateActiveScene }) {
   return (
     <>
-      <nav>
+      <nav className={style.navbar}>
         <Link to="/" onClick={() => updateActiveScene("home")}>
           Home
         </Link>
         <Link to="/aboutMe" onClick={() => updateActiveScene("aboutMe")}>
-          About Me
+          About
         </Link>
+        <div>Placeholder</div>
+        <div>Placeholder</div>
       </nav>
     </>
   );
--- src/Components/Pages/Home/index.js
+++ src/Components/Pages/Home/index.js
@@ -6,9 +6,21 @@ function Home({ updateActiveScene }) {
     <>
       <h1>HOME</h1>
       <NavBar updateActiveScene={updateActiveScene}></NavBar>
-      <div>Imagine there are some contents here</div>
+      <div>Hi!</div>
+      <div>Welcome to my brand new WIP personal website!</div>
+      <div>
+        You can view the code{" "}
+        <a
+          href="https://github.com/bM7tcHF88GBxDni/react-three-fiber-cannon-drei-007"
+          target="_blank"
+        >
+          here
+        </a>
+      </div>
+
+      <div>This is the main home page, this is all placeholder content!</div>
       <div>These are styled by flexbox css!</div>
-      <div>Some additional notes</div>
+      <div>Some additional text!</div>
     </>
   );
 }
