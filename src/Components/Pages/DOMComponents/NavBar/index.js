3c8e97b463ff7e5648884d5a0bfdf3db04b5132d
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-06-04 19:53:08 +0100

    Create new NavBar with @react-spring/web animations

--- src/Components/Pages/DOMComponents/NavBar/index.js
+++ src/Components/Pages/DOMComponents/NavBar/index.js
@@ -1,11 +1,52 @@
 import style from "./NavBar.module.css";
+import { animated, useSpring, config } from "@react-spring/web";
+import { useState } from "react";
 
-function NavBar({ updateActiveScene }) {
+function NavText({ updateActiveScene, text, scene }) {
+  const [animate, setAnimate] = useState(false);
+
+  function toggleOn() {
+    setAnimate(true);
+  }
+
+  function toggleOff() {
+    setAnimate(false);
+  }
+
+  const slideUp = useSpring({
+    from: { transform: "translateY(0%)" },
+    to: { transform: "translateY(-25%)" },
+    config: config.wobbly,
+    loop: true,
+    reset: true,
+  });
+
+  return (
+    <animated.h1
+      style={animate ? slideUp : null}
+      onClick={() => updateActiveScene(scene)}
+      onMouseEnter={toggleOn}
+      onMouseLeave={toggleOff}
+    >
+      {text}
+    </animated.h1>
+  );
+}
+
+export default function NavBar({ updateActiveScene }) {
   return (
     <>
       <nav className={style.navbar}>
-        <h1 onClick={() => updateActiveScene("home")}>H_ME_</h1>
-        <h1 onClick={() => updateActiveScene("skills")}>SKILLS_</h1>
+        <NavText
+          scene={"home"}
+          text={"H_ME_"}
+          updateActiveScene={updateActiveScene}
+        ></NavText>
+        <NavText
+          scene={"skills"}
+          text={"SKILLS_"}
+          updateActiveScene={updateActiveScene}
+        ></NavText>
         <h1>-PROJECTS</h1>
         <h1>ABOUT_ME</h1>
         <h1>C_NT_CT</h1>
@@ -13,5 +54,3 @@ function NavBar({ updateActiveScene }) {
     </>
   );
 }
-
-export default NavBar;
